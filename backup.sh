71d425c3ff83c72fa23ab1e94d5be0f7106e5173
iaga <alessio.iacarelli@wfp.org>
2020-10-22 06:40:45 +0200

    Add rclone backup steps

--- backup.sh
+++ backup.sh
@@ -3,6 +3,9 @@
 set -euo pipefail
 
 BORG_LOCATION="/borgbackup-database/borgbackup"
+RCLONE_SOURCE="/borgbackup-database"
+RCLONE_REMOTE_NAME="b2"
+B2_BUCKET_NAME="hoana-borgbackup"
 
 if pgrep borg > /dev/null; then
   echo "borg is already running - aborting"
@@ -18,3 +21,16 @@ borg create --stats --compression zstd,22 \
   /borgbackup-zai
 
 echo "Backup finished"
+echo "Running rclone.."
+
+if pgrep rclone > /dev/null; then
+  echo "rclone is already running - aborting"
+  exit 0
+fi
+
+rclone sync "${RCLONE_SOURCE}" "${RCLONE_REMOTE_NAME}":"${B2_BUCKET_NAME}"
+rclone cleanup "${RCLONE_REMOTE_NAME}":"${B2_BUCKET_NAME}"
+
+echo "Backblaze sync finished"
+
+echo "$(date): Backup completed" >> /var/log/cron.log 2>&1
