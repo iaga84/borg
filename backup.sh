54457234953ab985be93618a39a86c8c2add37bf
iaga <alessio.iacarelli@wfp.org>
2020-10-25 18:29:46 +0100

    Add more logging

--- backup.sh
+++ backup.sh
@@ -8,12 +8,20 @@ RCLONE_CONFIG="/borgbackup-database/rclone.conf"
 RCLONE_REMOTE_NAME="b2"
 B2_BUCKET_NAME="hoana-borgbackup"
 
+echo "$(date): Scheduled backup procedure started.." >> /var/log/cron.log 2>&1
+
 if pgrep borg > /dev/null; then
-  echo "borg is already running - aborting"
+  echo "$(date): borg is already running - aborting" >> /var/log/cron.log 2>&1
   exit 0
 fi
 
-echo "Running borgbackup.."
+if pgrep rclone > /dev/null; then
+  echo "$(date): rclone is already running - aborting" >> /var/log/cron.log 2>&1
+  exit 0
+fi
+
+echo "$(date): Running borgbackup" >> /var/log/cron.log 2>&1
+
 archive_name="borgbackup-$(date -Iseconds | cut -d '+' -f 1)"
 
 borg create --stats --compression zstd,22 \
@@ -21,17 +29,22 @@ borg create --stats --compression zstd,22 \
   /borgbackup-ale \
   /borgbackup-zai
 
-echo "Backup finished"
-echo "Running rclone.."
+echo "$(date): borgbackup completed, starting rclone" >> /var/log/cron.log 2>&1
+
+if pgrep borg > /dev/null; then
+  echo "$(date): borg is already running - aborting" >> /var/log/cron.log 2>&1
+  exit 0
+fi
 
 if pgrep rclone > /dev/null; then
-  echo "rclone is already running - aborting"
+  echo "$(date): rclone is already running - aborting" >> /var/log/cron.log 2>&1
   exit 0
 fi
 
 rclone sync --config="${RCLONE_CONFIG}" "${RCLONE_SOURCE}" "${RCLONE_REMOTE_NAME}":"${B2_BUCKET_NAME}"
+
+echo "$(date): rclone sync completed, starting rclone cleanup" >> /var/log/cron.log 2>&1
+
 rclone cleanup --config="${RCLONE_CONFIG}" "${RCLONE_REMOTE_NAME}":"${B2_BUCKET_NAME}"
 
-echo "Backblaze sync finished"
-
-echo "$(date): Backup completed" >> /var/log/cron.log 2>&1
+echo "$(date): Backup procedure successfully completed" >> /var/log/cron.log 2>&1
