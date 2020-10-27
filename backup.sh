324ac891d8d81c7a9530b047f4be40377df7e051
iaga <alessio.iacarelli@wfp.org>
2020-10-27 06:28:45 +0100

    Add logging to borgbackup

--- backup.sh
+++ backup.sh
@@ -7,44 +7,46 @@ RCLONE_SOURCE="/borgbackup-database"
 RCLONE_CONFIG="/borgbackup-database/rclone.conf"
 RCLONE_REMOTE_NAME="b2"
 B2_BUCKET_NAME="hoana-borgbackup"
+LOG_FILE="/var/log/cron.log"
 
-echo "$(date): Scheduled backup procedure started.." >> /var/log/cron.log 2>&1
+echo "$(date): Scheduled backup procedure started.." >> "${LOG_FILE}" 2>&1
 
 if pgrep borg > /dev/null; then
-  echo "$(date): borg is already running - aborting" >> /var/log/cron.log 2>&1
+  echo "$(date): borg is already running - aborting" >> "${LOG_FILE}" 2>&1
   exit 0
 fi
 
 if pgrep rclone > /dev/null; then
-  echo "$(date): rclone is already running - aborting" >> /var/log/cron.log 2>&1
+  echo "$(date): rclone is already running - aborting" >> "${LOG_FILE}" 2>&1
   exit 0
 fi
 
-echo "$(date): Running borgbackup" >> /var/log/cron.log 2>&1
+echo "$(date): Running borgbackup" >> "${LOG_FILE}" 2>&1
 
 archive_name="borgbackup-$(date -Iseconds | cut -d '+' -f 1)"
 
 borg create --stats --compression zstd,22 \
   "${BORG_LOCATION}"::"${archive_name}" \
   /borgbackup-ale \
-  /borgbackup-zai
+  /borgbackup-zai \
+  &> "${LOG_FILE}"
 
-echo "$(date): borgbackup completed, starting rclone" >> /var/log/cron.log 2>&1
+echo "$(date): borgbackup completed, starting rclone" >> "${LOG_FILE}" 2>&1
 
 if pgrep borg > /dev/null; then
-  echo "$(date): borg is already running - aborting" >> /var/log/cron.log 2>&1
+  echo "$(date): borg is already running - aborting" >> "${LOG_FILE}" 2>&1
   exit 0
 fi
 
 if pgrep rclone > /dev/null; then
-  echo "$(date): rclone is already running - aborting" >> /var/log/cron.log 2>&1
+  echo "$(date): rclone is already running - aborting" >> "${LOG_FILE}" 2>&1
   exit 0
 fi
 
 rclone sync --config="${RCLONE_CONFIG}" --bwlimit 0.275M "${RCLONE_SOURCE}" "${RCLONE_REMOTE_NAME}":"${B2_BUCKET_NAME}"
 
-echo "$(date): rclone sync completed, starting rclone cleanup" >> /var/log/cron.log 2>&1
+echo "$(date): rclone sync completed, starting rclone cleanup" >> "${LOG_FILE}" 2>&1
 
 rclone cleanup --config="${RCLONE_CONFIG}" "${RCLONE_REMOTE_NAME}":"${B2_BUCKET_NAME}"
 
-echo "$(date): Backup procedure successfully completed" >> /var/log/cron.log 2>&1
+echo "$(date): Backup procedure successfully completed" >> "${LOG_FILE}" 2>&1
