6079a44eba52aaebe521c5767cf467a446c452d6
iaga <alessio.iacarelli@wfp.org>
2020-10-28 07:11:22 +0100

    Change to absolute paths

--- backup.sh
+++ backup.sh
@@ -25,7 +25,7 @@ echo "$(date): Running borgbackup" >> "${LOG_FILE}" 2>&1
 
 archive_name="borgbackup-$(date -Iseconds | cut -d '+' -f 1)"
 
-borg create --stats --compression zstd,22 \
+/usr/local/bin/borg create --stats --compression zstd,22 \
   "${BORG_LOCATION}"::"${archive_name}" \
   /borgbackup-ale \
   /borgbackup-zai \
@@ -43,10 +43,10 @@ if pgrep rclone > /dev/null; then
   exit 0
 fi
 
-rclone sync --config="${RCLONE_CONFIG}" --bwlimit 0.275M "${RCLONE_SOURCE}" "${RCLONE_REMOTE_NAME}":"${B2_BUCKET_NAME}"
+/usr/bin/rclone sync --config="${RCLONE_CONFIG}" --bwlimit 0.275M "${RCLONE_SOURCE}" "${RCLONE_REMOTE_NAME}":"${B2_BUCKET_NAME}"
 
 echo "$(date): rclone sync completed, starting rclone cleanup" >> "${LOG_FILE}" 2>&1
 
-rclone cleanup --config="${RCLONE_CONFIG}" "${RCLONE_REMOTE_NAME}":"${B2_BUCKET_NAME}"
+/usr/bin/rclone cleanup --config="${RCLONE_CONFIG}" "${RCLONE_REMOTE_NAME}":"${B2_BUCKET_NAME}"
 
 echo "$(date): Backup procedure successfully completed" >> "${LOG_FILE}" 2>&1
